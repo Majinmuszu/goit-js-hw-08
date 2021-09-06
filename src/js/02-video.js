5b91fecc432759549aee1e09cdd9558f7143137f
Artur Kraśniewski <artur.krasniewski1@gmail.com>
2021-09-06 22:54:51 +0200

    2/3 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import throttle from 'lodash/throttle';
+import Player from '@vimeo/player';
+
+const qs = (selector) => document.querySelector(selector);
+const qsa = (selector) => document.querySelectorAll(selector);
+const log = (something) => console.log(something);
+const ce = (elem) => document.createElement(elem);
+
+const iframe = qs('iframe');
+const player = new Vimeo.Player(iframe);
+player.on('play', function() {
+    console.log('played the video!');
+});
+player.on('timeupdate', throttle((e) => {
+    log(e.seconds)
+    localStorage.setItem('videoplayer-current-time',`${e.seconds}`)
+  }, 2000));
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
 
